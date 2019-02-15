743db4d8a4dfbf5a1167aa720c05463ed469a82a
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 10:54:52 -0800

    task5A

--- task5A.py
+++ task5A.py
@@ -9,11 +9,11 @@ def start_with_vow():
             res = line.lower().strip(string.punctuation + string.whitespace$
             reserve = reserve + res
         count = 0
-        num_vowels=0
+        vowels=0
         for char in w:
            if char in "aeiouAEIOU":
-           num_vowels = num_vowels+1
-          
+           vowels = vowels+1
+           return vowels           
          
 
 
