5b798cafb89bdb2570c067b1f5f0bde8a58d4860
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 11:09:48 -0800

    task31.py

--- task31.py
+++ task31.py
@@ -1,10 +1,16 @@
 import requests
 import json
+def GetToken():
+    url = "https://sandboxapicem.cisco.com/api/v1/ticket"
+    data = {}
+    data['username'] = "devnetuser"
+    data['password'] = "Cisco123!"
+    data= json.dumps(data)
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=data,headers=headers)
+    return response.json()
 
-  url= "sandboxapicem.cisco.com"
-  username = "devnetuser"
-  password = "Cisco123!"
-  version = "v1"
-  content-type = "json/application"
+print(GetToken()) 
+
 
 
