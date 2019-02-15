5be3a2d893d6425c8a28222f1cdb5c32b4618131
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 10:51:14 -0800

    task5A

---
+++ task5A.py
@@ -0,0 +1,16 @@
+import string
+
+def start_with_vow():
+    
+    f = open('Book1.txt', 'r')
+        w = f.readlines()
+
+        for line in w:
+            res = line.lower().strip(string.punctuation + string.whitespace$
+            reserve = reserve + res
+        count = 0
+        num_vowels=0
+         
+         
+
+
