69a0977713ac348f19944b46d2d9cb3e133af881
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 10:07:25 -0800

    task1A

--- task1A.py
+++ task1A.py
@@ -1,10 +1,19 @@
 import string
-
 def unique_words():
+        reserve = []
+        f = open('Book1.txt','r')
 
-  f = open('Book1.txt','r')
-  list = []
-  f = f.strip()
+        for line in f:
+             res = line.lower().strip(string.punctuation + string.whitespace) 
+             reserve += res
+        return reserve
+            if words >= 50:
+              return found
+            else
+              return not found
 
-    for word in f:
-      
+print(unique_words())
+
+    
+ 
+     
