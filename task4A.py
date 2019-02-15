573f77a127da366e8ee7b2f801dad8a1cb98dfca
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 10:37:25 -0800

    task4A

---
+++ task4A.py
@@ -0,0 +1,26 @@
+import string
+
+def character_word_count(text):
+    
+    f = open('Book1.txt', 'r')
+        w = f.readlines()
+
+        for line in w:
+                res = line.lower().strip(string.punctuation + string.whitespace$
+                reserve = reserve + res
+         List = text.lower().split('.')
+         counter = 0
+         index = 0
+         Dict = {}
+         count = []
+        for character in List:
+           words = character.split()
+        for word in words:
+           index = words.index(word)
+           countList.append(index)
+        if word not in Dict:
+            Dict[word] = list()
+        else:
+            Dict[word].append(index)
+    
+   
