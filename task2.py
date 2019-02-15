4825d0eecc2adc3fabf7f3dcb4ac68e40538252c
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-02-15 11:25:18 -0800

    task2

---
+++ task2.py
@@ -0,0 +1,14 @@
+log=open('access.log','r')
+
+logget=open('get.log','a')
+logput=open('put.log','a')
+logpost=open('post.log','a')
+logdelete=open('delete.log','a')
+
+for line in log:
+        line=line.strip()
+        if 'get '.upper() in line.upper():
+                logget.write(line)
+        elif 'post '.upper() in line.upper():
+                logpost.write(line)
+      
